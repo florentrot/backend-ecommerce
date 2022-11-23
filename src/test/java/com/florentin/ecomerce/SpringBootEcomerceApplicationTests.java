d85ce599dc2c03b4b686fd86b7ac79d83f78cb3a
florentinr <florentinr@operative.com>
2022-11-23 17:53:05 +0200

    v1.0 spring part

---
+++ src/test/java/com/florentin/ecomerce/SpringBootEcomerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.florentin.ecomerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEcomerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
